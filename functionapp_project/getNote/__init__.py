d8ddab9d8ade0d6469c86cb753625c20a9a02a24
BeaBea1234 <33372871+BeaBea1234@users.noreply.github.com>
2022-08-01 17:05:44 +0200

    Update __init__.py

--- functionapp_project/getNote/__init__.py
+++ functionapp_project/getNote/__init__.py
@@ -9,7 +9,7 @@ def main(req: func.HttpRequest) -> func.HttpResponse:
     id = req.params.get('id')
     if id:
         try:
-            url = os.environ["MyDbConnection"] # Change the Variable name, as applicable to you
+            url = "mongodb://lab2cosmosdb:QAUk1MHvjnVfFuC6zqpBE3jU8o2ZUlV5JY5HnDvySTPqL5Qw45iduqeKaDE0n715sKNvFL30AugvjfsgN0OJjQ==@lab2cosmosdb.mongo.cosmos.azure.com:10255/?ssl=true&replicaSet=globaldb&retrywrites=false&maxIdleTimeMS=120000&appName=@lab2cosmosdb@" #os.environ["MyDbConnection"] # Change the Variable name, as applicable to you
             client = pymongo.MongoClient(url)
             database = client['lab2db'] # Change the MongoDB name (don't choose the name of the CosmosDb)
             collection = database['notes']    # Change the collection name
