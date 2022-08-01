90bc514c164d79a72974c433dc60dba8af0c65ac
BeaBea1234 <33372871+BeaBea1234@users.noreply.github.com>
2022-08-01 17:05:25 +0200

    Update __init__.py

--- functionapp_project/getNotes/__init__.py
+++ functionapp_project/getNotes/__init__.py
@@ -7,7 +7,7 @@ from bson.json_util import dumps
 def main(req: func.HttpRequest) -> func.HttpResponse:
 
     try:
-        url = os.environ["MyDbConnection"] # Change the Variable name, as applicable to you
+        url = "mongodb://lab2cosmosdb:QAUk1MHvjnVfFuC6zqpBE3jU8o2ZUlV5JY5HnDvySTPqL5Qw45iduqeKaDE0n715sKNvFL30AugvjfsgN0OJjQ==@lab2cosmosdb.mongo.cosmos.azure.com:10255/?ssl=true&replicaSet=globaldb&retrywrites=false&maxIdleTimeMS=120000&appName=@lab2cosmosdb@" #os.environ["MyDbConnection"] # Change the Variable name, as applicable to you
         client = pymongo.MongoClient(url)
         database = client['lab2db'] # Change the MongoDB name (don't use the name of CosmosDB)
         collection = database['notes']    # Change the collection name
